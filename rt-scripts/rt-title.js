9a777b4a59eb06eb4b6010f1289717b45e26754d
noc-sup <noc-sup@noc2.internal.labs.it>
2023-07-20 12:25:41 +0200

    Fixed RT-Title per CESIA; Rimesso a posto RT - WIP

--- rt-scripts/rt-title.js
+++ rt-scripts/rt-title.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name RT - Title
 // @description Script RT
-// @version 1.1
+// @version 1.2
 // @grant none
 // @author Endri
 // @match https://rt.labs.it/rt/Ticket/*
 // ==/UserScript==
 
 var t = document.getElementById('header');
+var queue = document.getElementsByClassName('queue')[0].getElementsByClassName('value');
 var text = t.getElementsByTagName('h1');
 
 for (var i = 0; i < text.length; i++){
@@ -17,5 +18,11 @@ for (var i = 0; i < text.length; i++){
   for (var j = 1; j < txtarray.length; j++){
     rest += txtarray[j];
   }
-  text[i].innerHTML = testo.concat(rest);
+  if ( queue[0].innerText == 'CESIA' ) {
+    rest += " ";
+    text[i].innerHTML = rest.concat(testo);
+  } else {
+    text[i].innerHTML = testo.concat(rest);
+  }
 }
+
--- rt-scripts/rt-wip.js
+++ rt-scripts/rt-wip.js
@@ -1,28 +1,34 @@
 // ==UserScript==
-// @name RT - Title
+// @name RT - WIP
 // @description Script RT
-// @version 1.2
+// @version 1.3
 // @grant none
-// @author Endri
-// @match https://rt.labs.it/rt/Ticket/*
+// @match https://rt.labs.it/rt/Dashboards/41326/*
 // ==/UserScript==
 
-var t = document.getElementById('header');
-var queue = document.getElementsByClassName('queue')[0].getElementsByClassName('value');
-var text = t.getElementsByTagName('h1');
+var trs = document.getElementsByTagName('tr');
+var user = document.getElementsByClassName('current-user');
 
-for (var i = 0; i < text.length; i++){
-  const txtarray = text[i].innerHTML.split(":");
-  var testo = "[LABS " + txtarray[0] + "]";
-  var rest = "";
-  for (var j = 1; j < txtarray.length; j++){
-    rest += txtarray[j];
-  }
-  if ( queue[0].innerText == 'CESIA' ) {
-    rest += " ";
-    text[i].innerHTML = rest.concat(testo);
-  } else {
-    text[i].innerHTML = testo.concat(rest);
+for (var i = 1; i <= trs.length; i++) {
+  var tds = trs[i].getElementsByTagName('td');
+
+  for (var l = 0; l < tds.length; l++) {
+    switch(String(tds[7].getElementsByTagName('span')[0].innerHTML)){
+      case "high":
+        tds[l].style.backgroundColor = "rgb(255, 115, 115)";
+        break;
+      case "medium":
+        tds[l].style.backgroundColor = "rgb(255, 197, 97)";
+        break;
+      case "low":
+        tds[l].style.backgroundColor = "rgb(255, 246, 120)";
+        break;
+      case "none":
+        tds[l].style.backgroundColor = "rgb(222, 222, 222)";
+        break;
+      default:
+        tds[l].style.backgroundColor = "rgb(222, 222, 222)";
+        break;
+    }
   }
 }
-
