24fa8726856445e27ebff20d214b1d84372b1793
noc-sup <noc-sup@noc2.internal.labs.it>
2023-07-20 10:44:06 +0200

    Aggiornato RT-WIP per CESIA

--- rt-scripts/rt-wip.js
+++ rt-scripts/rt-wip.js
@@ -1,35 +1,28 @@
 // ==UserScript==
-// @name RT - WIP
+// @name RT - Title
 // @description Script RT
-// @version 1.2
+// @version 1.1
 // @grant none
 // @author Endri
-// @match https://rt.labs.it/rt/Dashboards/41326/*
+// @match https://rt.labs.it/rt/Ticket/*
 // ==/UserScript==
 
-var trs = document.getElementsByTagName('tr');
-var user = document.getElementsByClassName('current-user');
+var t = document.getElementById('header');
+var queue = document.getElementsByClassName('queue')[0].getElementsByClassName('value');
+var text = t.getElementsByTagName('h1');
 
-for (var i = 1; i <= trs.length; i++) {
-  var tds = trs[i].getElementsByTagName('td');
-
-  for (var l = 0; l < tds.length; l++) {
-    switch(String(tds[7].getElementsByTagName('span')[0].innerHTML)){
-      case "high":
-        tds[l].style.backgroundColor = "rgb(255, 115, 115)";
-        break;
-      case "medium":
-        tds[l].style.backgroundColor = "rgb(255, 197, 97)";
-        break;
-      case "low":
-        tds[l].style.backgroundColor = "rgb(255, 246, 120)";
-        break;
-      case "none":
-        tds[l].style.backgroundColor = "rgb(222, 222, 222)";
-        break;
-      default:
-        tds[l].style.backgroundColor = "rgb(222, 222, 222)";
-        break;
-    }
+for (var i = 0; i < text.length; i++){
+  const txtarray = text[i].innerHTML.split(":");
+  var testo = "[LABS " + txtarray[0] + "]";
+  var rest = "";
+  for (var j = 1; j < txtarray.length; j++){
+    rest += txtarray[j];
+  }
+  if ( queue[0].innerText == 'CESIA' ) {
+    rest += " ";
+    text[i].innerHTML = rest.concat(testo);
+  } else {
+    text[i].innerHTML = testo.concat(rest);
   }
 }
+
